d387505a0e3c83ae7e4dcaa4af0e78cb1c7134b3
Viktor Potemkin <99296303+potemkinViktor@users.noreply.github.com>
2022-03-23 23:39:03 +0300

    Update Vault.sol

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,12 +1,14 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 abstract contract Vault is Ownable, IERC20 {
 
-    function withdraw(address to, uint256 _amount) public onlyOwner {
-       owner.transfer(to, _amount);
-    }
+    function withdraw(IERC20 token, address to, uint256 amount) public onlyOwner {
+        uint256 erc20balance = token.balanceOf(address(this));
+        require(amount <= erc20balance, "balance is low");
+        token.transfer(to, amount);
+    }    
 }
